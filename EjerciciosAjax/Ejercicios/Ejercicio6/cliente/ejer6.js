8e08edd26eb74bc280ee49383bc1215bb7fb1903
JesuLopezGomez <jelopez.gom@gmail.com>
2023-11-10 12:21:19 +0100

    completado hasta la mitad del 6

---
+++ EjerciciosAjax/Ejercicios/Ejercicio6/cliente/ejer6.js
@@ -0,0 +1,36 @@
+let municipio = document.getElementById("municipio");
+let containerDiv = document.getElementById("containerList");
+
+municipio.addEventListener("keyup",() => {
+    if(municipio.value){
+        autoCompleta(municipio.value);
+    }
+})
+
+async function autoCompleta(letra){
+    const formData = new FormData();
+    formData.append("municipio",letra);
+   
+    const response = await fetch("https://intranetjacaranda.es/Ejercicios/autocompletaMunicipios.php",{
+        method : "POST",
+        body : formData
+    });
+    
+    const municipios = await response.json();
+
+    if(document.querySelector("ul") != null){
+        document.querySelector("ul").remove();
+    }
+
+    let ul = document.createElement("ul");
+    ul.setAttribute("id","listaMunicipio");
+    containerDiv.appendChild(ul);
+
+    municipios.forEach(municipio => {
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(municipio));
+        ul.appendChild(li);
+    });
+}
+
+
